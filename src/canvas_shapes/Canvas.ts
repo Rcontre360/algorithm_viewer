be64dce7b93486062ad5c7548a7768b51da6165c
Rcontre360 <rcontreraspimentel@gmail.com>
2021-03-30 01:10:31 -0400

    Added nodes implemented

--- src/canvas_shapes/Canvas.ts
+++ src/canvas_shapes/Canvas.ts
@@ -1,11 +1,44 @@
 import {fabric} from "fabric"
 
-class Canvas extends fabric.Canvas{
+interface CanvasInterface extends fabric.Canvas{
+	allowAddNode():void,
+	forbidAddNode():void
+}
+
+class Canvas extends fabric.Canvas implements CanvasInterface {
+
+	private addNodeHandler: ((event: fabric.IEvent) => void) = (event:fabric.IEvent) => {console.log("epa")}
+
 	constructor(canvasId:string,canvasContainerId:string){
+		super(canvasId)
+
 		const canvasContainer = document.getElementById("canvas_container") as HTMLElement;
 		const { clientWidth, clientHeight } = canvasContainer
 
-		super(canvasId,{width:clientWidth,height:clientHeight})
+		this.setDimensions({ width: clientWidth, height: clientHeight })
+		this.on("mouse:down", this.addNodeHandler)
+	}
+	allowAddNode():void{
+		this.addNodeHandler = (event:fabric.IEvent):void => {
+			let { pointer } = event
+			const circle = new fabric.Circle({
+				fill: "red",
+				radius: 20,
+				top: pointer ? pointer.y : 100,
+				left: pointer ? pointer.x : 100,
+				lockMovementX: true,
+				lockMovementY: true,
+			})
+			this.add(circle)
+			console.log("circle added")
+		}
+		console.log("circle added")
+	}
+	forbidAddNode():void{
+		this.addNodeHandler = (event:fabric.IEvent):void=>{}
+		this.on("event:dragover",event=>{
+			console.log("dragover",event)
+		})
 	}
 }
 
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,70 @@
-import React,{useEffect} from "react"
+import React,{useState,useEffect} from "react"
 import { applyMiddleware, createStore } from 'redux'
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {makeStyles} from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
 import { useSelector } from 'react-redux';
 import {fabric} from "fabric"
 import {Canvas} from "../canvas_shapes"
 
+const useStyles = makeStyles((theme)=>({
+	canvasContainer:{
+		width:"100%",
+		height:"70vh",
+	}
+}))
+
 const App = ()=>{
+	const [addNode,setAddNode] = useState<boolean>(false)
+	const [canvas,setCanvas] = useState<Canvas | null>(null)
+	const classes = useStyles()
 
 	useEffect(()=>{
-		const circle = new fabric.Circle({
-			fill:"red",
-			radius:20,
-			top:100,
-			left:100
-		})
-		const testCanvas = new Canvas("main_canvas","canvas_container")
-		testCanvas.add(circle)
-
+		setCanvas(new Canvas("main_canvas","canvas_container"))
 	},[])
 
+	useEffect(()=>{
+		if (canvas) {
+			if (addNode)
+				canvas.allowAddNode()
+			else
+				canvas.forbidAddNode()
+		}
+	},[canvas,addNode])
+
 	return (
-	<div>
-		<h1>from typescript react</h1>
-		<div id="canvas_container">
+	<Box
+		display="flex"
+		flexDirection="column"
+		justifyContent="center"
+		alignItems="center"
+	>
+		<Typography
+			variant="h3"
+			component="h1"
+		>
+			Algorithm Previewer
+		</Typography>
+		<Box>
+			<Button
+				onClick={()=>setAddNode(prev=>!prev)}
+				variant="contained"
+				color="primary"
+			>
+				Add node
+			</Button>
+			<Button>Option 2</Button>
+			<Button>Option 3</Button>
+		</Box>
+		<div 
+			id="canvas_container" 
+			className={classes.canvasContainer}
+		>
 			<canvas id="main_canvas"></canvas>
 		</div>
-	</div>
+	</Box>
 	)
 }
 
