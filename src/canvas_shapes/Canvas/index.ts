bc92f73053f0b0a6bf96244fe1be747a49e76c8b
Rcontre360 <rcontreraspimentel@gmail.com>
2021-04-01 20:21:19 -0400

    Added drawer for lines
    Drawer for straight lines and arrow functions inside fields

--- src/canvas_shapes/Canvas/index.ts
+++ src/canvas_shapes/Canvas/index.ts
@@ -1,39 +1,49 @@
-import {fabric} from "fabric"
+import {
+	fabric
+} from "fabric"
 import lineDrawer from "./lineDrawer"
 
-interface CanvasInterface extends fabric.Canvas{
-	allowAddNode():void,
-	forbidAddNode():void
+interface CanvasInterface extends fabric.Canvas {
+	allowAddNode(): void,
+		forbidAddNode(): void
 }
 
 interface lineDrawerInterface {
-	setCanvas(canvas:fabric.Canvas):void,
-	setDrawingEvent():void
+	setCanvas(canvas: fabric.Canvas): void,
+		setDrawingEvent(): void
 }
 
 class Canvas extends fabric.Canvas implements CanvasInterface {
+	private drawingLine: boolean = false
+	private drawer: lineDrawer | null = null
 
-	private drawingLine:boolean = false
-	private currentLine:fabric.Line | null = null
-
-	constructor(canvasId:string,canvasContainerId:string){
+	constructor(canvasId: string, canvasContainerId: string) {
 		super(canvasId)
 
 		const canvasContainer = document.getElementById("canvas_container") as HTMLElement;
-		const { clientWidth, clientHeight } = canvasContainer
+		const {
+			clientWidth,
+			clientHeight
+		} = canvasContainer
 
-		this.setDimensions({ width: clientWidth, height: clientHeight })
+		this.setDimensions({
+			width: clientWidth,
+			height: clientHeight
+		})
+		this.drawer = new lineDrawer(this)
 	}
 
-	allowAddNode():void{
-		this.on("mouse:down",this.addNodeHandler)
+	allowAddNode = () => {
+		this.on("mouse:down", this.addNodeHandler)
 	}
 
-	forbidAddNode():void{
-		this.off("mouse:down",this.addNodeHandler)
+	forbidAddNode = (allowAddEdges ? : boolean) => {
+		this.off("mouse:down", this.addNodeHandler)
+		if (allowAddEdges && this.drawer)
+			this.drawer.setDrawingEvents()
 	}
 
-	private addNodeHandler(event:fabric.IEvent){
+	private addNodeHandler = (event: fabric.IEvent) => {
 		const pointer = event.pointer as fabric.Point
 		const circle = new fabric.Circle({
 			fill: "red",
@@ -45,7 +55,6 @@ class Canvas extends fabric.Canvas implements CanvasInterface {
 		})
 		this.add(circle)
 	}
-
 }
 
 export {
